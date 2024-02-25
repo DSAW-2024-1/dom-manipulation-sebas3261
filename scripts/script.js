d1c72173149646428e736d9b0c282870482fafc3
sebas3261 <sebassanchez3261@gmail.com>
2024-02-25 13:48:42 -0500

    dessign2

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-console.log("JavaScript working!");
+document.addEventListener('DOMContentLoaded', function() {
+    const myDay = document.getElementById('myDay');
+    const box1 = document.getElementById('box1');
+    const main = document.getElementById('main');
+
+    myDay.addEventListener('click', function(event){
+        const ancho = window.innerWidth;
+        if(ancho < 768)
+        {
+            box1.style.display = 'none';
+            main.style.display = 'flex';
+        }
+        else
+        {
+            console.log("desktop");
+        }
+    });
+});
